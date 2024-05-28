14646416d5c7e575abad9501d80a99da6aef48d9
山本翠 <yamamotosui@yamamotomidorinoMacBook-Air.local>
2024-05-28 16:43:25 +0900

    todo詳細画面の作成

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,4 +3,14 @@ class TodosController < ApplicationController
     @user = current_user
     @todos = @user.todos.order(created_at: :desc)
   end
+
+  def show
+    @todo = current_user.todos.find(params[:id])
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:title, :content)
+  end
 end
